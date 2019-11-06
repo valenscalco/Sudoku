5232f87e6cc5bae1c09a391790d70137eb7bfa33
valenscalco <mv.scalco@alumno.um.edu.ar>
2019-11-06 15:49:37 -0300

    commit

--- interfaz.py
+++ interfaz.py
@@ -11,6 +11,8 @@ class Interfaz(Sudoku):
         u = 0
         for i in range(1):
             print('\t')
+            print("Para salir presione ENTER")
+            print('\t')
         for j in range(9):
             u += 1
             if u == 4 or u == 7:
@@ -30,14 +32,21 @@ class Interfaz(Sudoku):
             while valido is False:
                 number = int(input("Ingrese un numero entre el 1 al 9\n"))
                 valido = Interfaz.validate(number)
+                if valido is False:
+                    print("Ingrese un numero válido")
             while validor is False:
                 x = int(input(
                     "Ingrese la fila para colocar el numero del 0 al 8 \n"))
                 validor = self.validate_x(x)
+                if validor is False:
+                    print("Ingrese un numero válido")
             while validoc is False:
                 y = int(input(
                     "Ingrese la columna para colocar el numero del 0 al 8 \n"))
                 validoc = self.validate_y(y)
+                if validoc is False:
+                    print("Ingrese un numero válido")
+            print("Posición no válida")
 
             self.sudoku.val_variables(number, x, y)
 
@@ -46,7 +55,6 @@ class Interfaz(Sudoku):
             if number >= 1 and number <= 9:
                 return True
             else:
-                print("Ingrese un numero entre 0 y 9")
                 return False
         except Exception:
             return False
@@ -56,10 +64,8 @@ class Interfaz(Sudoku):
             if x >= 0 and x <= 8:
                 return True
             else:
-                print("Ingrese un numero entre 0 y 8")
                 return False
         except Exception:
-            print("Ingrese un numero porfavor")
             return False
 
     def validate_y(self, y):
@@ -67,21 +73,9 @@ class Interfaz(Sudoku):
             if y >= 0 and y <= 8:
                 return True
             else:
-                print("Ingrese un numero entre 0 y 8")
                 return False
         except Exception:
-            print("Ingrese un numero porfavor")
             return False
-
-    def why(self, number, x, y):
-        if self.sudoku.val_area(number, x, y) is False:
-            print("Se repite el numero en el area")
-        if self.sudoku.val_column(number, y) is False:
-            print("Se repite el numero en la columna")
-        if self.sudoku.val_row(number, x) is False:
-            print("Se repite el numero en la fila")
-        if self.sudoku.val_position(x, y) is False:
-            print("Posicion original, coloque otra posicion")
 
 
 if __name__ == "__main__":
